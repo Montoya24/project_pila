5fa1e2ef2733f8960d8948b5eda89847d18c28e7
Montoya24 <145078948+Montoya24@users.noreply.github.com>
2023-09-17 02:33:59 +0000

    ⭐ Enhance: App

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
+//cargando dependencias
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var debug = require('debug')('project_pila:server');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+//creando la instancia de express
 var app = express();
 
-// view engine setup
+// configurando el motor de plantillas
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+/* GET users. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.render('author',{author: "Espinoza Montoya Aylen"});
 });
 
 module.exports = router;

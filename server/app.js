f058d470aefc5ba54ca07ed392d9590661dbbe09
Montoya24 <145078948+Montoya24@users.noreply.github.com>
2023-09-17 02:46:15 +0000

    ⭐ Feature: Server Directory

--- server/app.js
+++ server/app.js
@@ -6,8 +6,9 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var debug = require('debug')('project_pila:server');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+
+var indexRouter = require('../routes/index');
+var usersRouter = require('../routes/users');
 
 //creando la instancia de express
 var app = express();
@@ -16,15 +17,19 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
+//se establecen los middlewares
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+//crea un server de archivos estaticos
+app.use(express.static(path.join(__dirname, '..', 'public')));
 
+//registro de middlewares de aplicacion
 app.use('/', indexRouter);
+//Activar "usersRouter" cuando se 
+//solicita "/users"
 app.use('/users', usersRouter);
-
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
